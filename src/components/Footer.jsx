a258f73890412acd8899703983f7cc0d9fc28997
Sadan Mian <sadanmiyan007@gmail.com>
2023-08-10 22:32:40 +0530

    Newsletter

---
+++ src/components/Footer.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import { styled } from "styled-components";
+
+const Container = styled.div``;
+
+export const Footer = () => {
+  return <Container>Footer</Container>;
+};
