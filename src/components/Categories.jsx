048c4de51333ba7572de2c83e8e2dd02f744c246
Sadan Mian <sadanmiyan007@gmail.com>
2023-08-10 20:31:08 +0530

    CategoryItem

---
+++ src/components/Categories.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import { styled } from "styled-components";
+import { CategoryItem } from "./CategoryItem";
+import { categories } from "../data";
+
+const Conatiner = styled.div`
+  display: flex;
+  padding: 20px;
+  justify-content: space-between;
+`;
+
+export const Categories = () => {
+  return (
+    <Conatiner>
+      {categories.map((item) => (
+        <CategoryItem item={item} />
+      ))}
+    </Conatiner>
+  );
+};
