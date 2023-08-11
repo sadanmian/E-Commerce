83ca4b97d49cbc2862c71272b325dbd94f36bfe1
Sadan Mian <sadanmiyan007@gmail.com>
2023-08-11 16:10:10 +0530

    Login and Register Page

--- src/App.js
+++ src/App.js
@@ -2,13 +2,17 @@ import React from "react";
 import { Home } from "./pages/Home";
 import { ProductList } from "./pages/ProductList";
 import { Product } from "./pages/Product";
+import { Register } from "./pages/Register";
+import { Login } from "./pages/Login";
 
 function App() {
   return (
     <div>
       {/* <Home /> */}
       {/* <ProductList /> */}
-      <Product />
+      {/* <Product /> */}
+      {/* <Register /> */}
+      <Login />
     </div>
   );
 }
